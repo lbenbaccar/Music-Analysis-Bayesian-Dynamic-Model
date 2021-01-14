95351863fe4cccb738a61fd53e126eb2102a74e2
erwanrh <48155512+erwanrh@users.noreply.github.com>
2021-01-14 21:02:43 +0100

    update

--- Librosa_Music_Preprocess.py
+++ Librosa_Music_Preprocess.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python3
+#!\usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
 Created on Wed Jan  6 21:58:14 2021
@@ -12,28 +12,47 @@ import librosa
 import librosa.display
 from librosa.feature import mfcc
 import seaborn as sns
-import matplotlib as plt
-
+import matplotlib.pyplot as plt
+import numpy as np
+import scipy
 
 # %% Visualization
 
-path_temp =  '' #Mettre le mP3 ici
+path_temp =  'Documents/Cours/MSDS/S1/Statistique Bayesienne/MusicAnalysis-Bayesian_DynamicModel.nosync/Data/beatles.mp3' #Mettre le mP3 ici
 
 amplitude, sr = librosa.load(path_temp)
 print(amplitude.shape)
 
-librosa.display.waveplot(amplitude, song, alpha=0.8)
-song_melspecto = librosa.feature.melspectrogram(amplitude, s)
-sbd = librosa.power_to_db(song_melspecto)
+# %% Waveplots
+librosa.display.waveplot(amplitude, sr, alpha=0.8)
+song_melspecto = librosa.feature.melspectrogram(amplitude, sr)
+sbd = librosa.power_to_db(song_melspectro)
 
 # %% Decibel graphs
-librosa.display.specshow(sbd, sr=song, hop_length=500, x_axis='time', y_axis='log')
+librosa.display.specshow(sbd, sr=sr, hop_length=500, x_axis='time', y_axis='log')
+# %%  Show melspectogram
+librosa.display.specshow(song_melspecto, sr=sr, hop_length=500, x_axis='time', y_axis='log')
+
+# %%
+#mfccs = mfcc(sr=sr, y=amplitude, n_mfcc=40)
+librosa.display.specshow(sbd, x_axis='time', y_axis='log')
+
+#%% Test de la fonction mfcc
+mfccs = librosa.feature.mfcc(y=amplitude, sr=sr, n_mfcc=40)
+
+# %% Graph des mfcc
+librosa.display.specshow(mfccs, x_axis='time', y_axis='log')
 
 
-# Utilsier la fonction mfcc pour extraire les paramètres now 
-mfcc(sr=22050, S=sbd, n_mfcc=40)
-plt.figure(figsize=(10, 4))
-librosa.display.specshow(mfccs, x_axis='time')
-plt.colorbar()
-plt.title('MFCC')
-plt.tight_layout()
+# %% Vector Quantization
+whiten  = scipy.cluster.vq.whiten(mfccs)
+codebook,_ = scipy.cluster.vq.kmeans(whiten, 16 )
+# %% Plot des kmeans
+plt.scatter(whiten[:,20], whiten[:,21])
+plt.scatter(codebook[:,20], codebook[:,21])
+
+#%% VQ
+code, _ = scipy.cluster.vq.vq(whiten, codebook)
+
+#%% VQ Plot
+plt.plot(code)
