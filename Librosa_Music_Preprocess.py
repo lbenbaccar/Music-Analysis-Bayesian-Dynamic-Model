9c69972991629f234c0d8446f4a089d5cfd6c91e
erwanrh <48155512+erwanrh@users.noreply.github.com>
2021-01-15 02:10:50 +0100

    Update_Erwan
    Clean code + Ajout notebook pour plts

--- Librosa_Music_Preprocess.py
+++ Librosa_Music_Preprocess.py
@@ -6,7 +6,7 @@ Created on Wed Jan  6 21:58:14 2021
 @author: erwanrahis
 """
 
-# %% Imports
+# %% Libraries
 import sys, os
 import librosa
 import librosa.display
@@ -16,43 +16,49 @@ import matplotlib.pyplot as plt
 import numpy as np
 import scipy
 
-# %% Visualization
-
+# %% Loading the song
+#Path to the song
 path_temp =  'Documents/Cours/MSDS/S1/Statistique Bayesienne/MusicAnalysis-Bayesian_DynamicModel.nosync/Data/beatles.mp3' #Mettre le mP3 ici
-
+#Loading using librosa
 amplitude, sr = librosa.load(path_temp)
-print(amplitude.shape)
-
-# %% Waveplots
+#%%Plot of the amplitude
+plt.figure(figsize=(10, 4))
 librosa.display.waveplot(amplitude, sr, alpha=0.8)
-song_melspecto = librosa.feature.melspectrogram(amplitude, sr)
-sbd = librosa.power_to_db(song_melspectro)
-
-# %% Decibel graphs
-librosa.display.specshow(sbd, sr=sr, hop_length=500, x_axis='time', y_axis='log')
-# %%  Show melspectogram
-librosa.display.specshow(song_melspecto, sr=sr, hop_length=500, x_axis='time', y_axis='log')
-
-# %%
-#mfccs = mfcc(sr=sr, y=amplitude, n_mfcc=40)
-librosa.display.specshow(sbd, x_axis='time', y_axis='log')
-
-#%% Test de la fonction mfcc
-mfccs = librosa.feature.mfcc(y=amplitude, sr=sr, n_mfcc=40)
-
-# %% Graph des mfcc
-librosa.display.specshow(mfccs, x_axis='time', y_axis='log')
+plt.title('Waveform : amplitude ')
 
 
-# %% Vector Quantization
+# %% Melspectrograme
+song_melspecto = librosa.feature.melspectrogram(y=amplitude, sr=sr)
+#Convert from power to decibels
+sbd = librosa.power_to_db(song_melspecto, ref=np.max)
+#Plot mel spectrogram
+plt.figure(figsize=(10, 4))
+librosa.display.specshow(sbd, sr=sr, hop_length=500, x_axis='time', y_axis='mel')
+plt.title('Mel spectrogram')
+plt.colorbar(format='%+2.0f dB')
+plt.tight_layout()
+
+
+#%% Compute MFCCs
+mfccs = librosa.feature.mfcc(y=amplitude, sr=sr, n_mfcc=6000)
+#Plot MFCCs
+plt.figure(figsize=(10,4))
+librosa.display.specshow(mfccs, hop_length=500, x_axis='time')
+plt.title('MFCCs')
+plt.colorbar()
+
+# %% Clustering (kmeans)
+#Standardize the columns
 whiten  = scipy.cluster.vq.whiten(mfccs)
-codebook,_ = scipy.cluster.vq.kmeans(whiten, 16 )
-# %% Plot des kmeans
-plt.scatter(whiten[:,20], whiten[:,21])
-plt.scatter(codebook[:,20], codebook[:,21])
+#Creates 16 code books
+codebook,_ = scipy.cluster.vq.kmeans(whiten, 2400)
 
-#%% VQ
+# %% Vector Quantization (VQ)
 code, _ = scipy.cluster.vq.vq(whiten, codebook)
-
-#%% VQ Plot
+#Plot
 plt.plot(code)
+
+#%% Time split
+time = np.arange(0,len(amplitude))/(60*sr) #Convert in minutes
+
+plt.plot(time, amplitude, alpha=0.8)
