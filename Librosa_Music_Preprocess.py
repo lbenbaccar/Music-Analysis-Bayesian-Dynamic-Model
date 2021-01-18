2832d0f1a7d852f8d007f1867c280b51486281e6
erwanrh <48155512+erwanrh@users.noreply.github.com>
2021-01-18 16:15:50 +0100

    update

--- Librosa_Music_Preprocess.py
+++ Librosa_Music_Preprocess.py
@@ -16,18 +16,20 @@ import matplotlib.pyplot as plt
 import numpy as np
 import scipy
 
+
 # %% Loading the song
 #Path to the song
-path_temp =  'Documents/Cours/MSDS/S1/Statistique Bayesienne/MusicAnalysis-Bayesian_DynamicModel.nosync/Data/beatles.mp3' #Mettre le mP3 ici
+path_temp =  'Documents/Cours/MSDS/S1/Statistique Bayesienne/MusicAnalysis-Bayesian_DynamicModel.nosync/Data/beatles.wav' #Mettre le mP3 ici
 #Loading using librosa
 amplitude, sr = librosa.load(path_temp)
 
-#%%Plot of the amplitude
+
+#%%Plot of the amplitude using LIBROSA
 plt.figure(figsize=(10, 4))
 librosa.display.waveplot(amplitude, sr, alpha=0.8)
 plt.title('Waveform : amplitude ')
 
-# %% Melspectrograme
+# %% Melspectrograme LIBROSA
 song_melspecto = librosa.feature.melspectrogram(y=amplitude, sr=sr)
 #Convert from power to decibels
 sbd = librosa.power_to_db(song_melspecto, ref=np.max)
@@ -40,27 +42,16 @@ plt.tight_layout()
 
 
 #%% Compute MFCCs
-mfccs = librosa.feature.mfcc(y=amplitude, sr=sr, n_mfcc=6000)
-#Plot MFCCs
-plt.figure(figsize=(10,4))
-librosa.display.specshow(mfccs, hop_length=500, x_axis='time')
-plt.title('MFCCs')
-plt.colorbar()
+mfccs = librosa.feature.mfcc(y=amplitude, sr=sr, n_mfcc=10, norm="ortho")
+plt.figure(figsize=(10, 5))
+plt.imshow(mfccs,cmap=plt.cm.jet, aspect='auto')
 
-# %% Clustering (kmeans)
-#Standardize the columns
-whiten  = scipy.cluster.vq.whiten(mfccs)
-#Creates 16 code books
-codebook,_ = scipy.cluster.vq.kmeans(whiten, 16)
-
-# %% Vector Quantization (VQ)
-code, _ = scipy.cluster.vq.vq(whiten, codebook)
-#Plot
-plt.plot(code)
-
+librosa.display.specshow(mfccs, sr=sr, hop_length=500, x_axis='time')
 #%% Time conversion
 time = np.arange(0,len(amplitude))/(60*sr) #Convert in minutes
 plt.plot(time, amplitude, alpha=0.8)
+
+
 
 #%% Split time
 frame_100ms = int(sr/10)*20
