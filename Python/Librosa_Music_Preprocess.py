adbb43c66e462780bde5c81ced21aee8dca9c1c9
erwanrh <48155512+erwanrh@users.noreply.github.com>
2021-01-30 14:12:30 +0100

    update

---
+++ Python/Librosa_Music_Preprocess.py
@@ -0,0 +1,77 @@
+#!\usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jan  6 21:58:14 2021
+
+@author: erwanrahis
+"""
+
+# %% Libraries
+import sys, os
+import librosa
+import librosa.display
+from librosa.feature import mfcc
+import seaborn as sns
+import matplotlib.pyplot as plt
+import numpy as np
+import scipy
+
+
+# %% Loading the song
+#Path to the song
+path_temp =  'Documents/Cours/MSDS/S1/Statistique Bayesienne/MusicAnalysis-Bayesian_DynamicModel.nosync/Data/beatles.wav' #Mettre le mP3 ici
+#Loading using librosa
+amplitude, sr = librosa.load(path_temp)
+
+
+#%%Plot of the amplitude using LIBROSA
+plt.figure(figsize=(10, 4))
+librosa.display.waveplot(amplitude, sr, alpha=0.8)
+plt.title('Waveform : amplitude ')
+
+# %% Melspectrograme LIBROSA
+song_melspecto = librosa.feature.melspectrogram(y=amplitude, sr=sr)
+#Convert from power to decibels
+sbd = librosa.power_to_db(song_melspecto, ref=np.max)
+#Plot mel spectrogram
+plt.figure(figsize=(10, 4))
+librosa.display.specshow(sbd, sr=sr, hop_length=500, x_axis='time', y_axis='mel')
+plt.title('Mel spectrogram')
+plt.colorbar(format='%+2.0f dB')
+plt.tight_layout()
+
+
+#%% Compute MFCCs
+mfccs = librosa.feature.mfcc(y=amplitude, sr=sr, n_mfcc=10, norm="ortho")
+plt.figure(figsize=(10, 5))
+plt.imshow(mfccs,cmap=plt.cm.jet, aspect='auto')
+
+librosa.display.specshow(mfccs, sr=sr, hop_length=500, x_axis='time')
+#%% Time conversion
+time = np.arange(0,len(amplitude))/(60*sr) #Convert in minutes
+plt.plot(time, amplitude, alpha=0.8)
+
+
+
+#%% Split time
+frame_100ms = int(sr/10)*20
+code_final=[]
+
+for i in range(0, len(amplitude)-frame_100ms, frame_100ms):
+    print(str(i+frame_100ms)+'/'+str(len(amplitude)))
+    i_ = i+frame_100ms
+    frame_amplitude = amplitude[i:i_]
+    frame_mfccs = librosa.feature.mfcc(y=frame_amplitude, sr=sr, n_mfcc=40)
+    frame_whiten  = scipy.cluster.vq.whiten(frame_mfccs)
+    frame_codebook,_ = scipy.cluster.vq.kmeans(frame_whiten, 16)
+    frame_code, _ = scipy.cluster.vq.vq(frame_whiten, frame_codebook)
+    code_final.append(frame_code)
+    
+test = np.concatenate(code_final)
+
+#%% Plot
+plt.figure(figsize=(10,4))
+plt.plot(test)
+
+
+
